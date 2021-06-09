cefbafe65faeea8b99250e54ac242c25f4ed5a93
AgneseBettio <bettio.agnese@gmail.com>
2021-06-09 18:31:44 +0200

    query 4

--- query_esercizio.sql
+++ query_esercizio.sql
@@ -15,7 +15,9 @@ WHERE YEAR(`date_of_birth`) > '1988';
 
 -- 3. Seleziona tutti gli ospiti che hanno più di 20 anni (al momento
 -- dell’esecuzione della query)
-
+SELECT * 
+FROM `ospiti` 
+WHERE YEAR(CURDATE()) - YEAR(`date_of_birth`) >= '20';
 
 
 -- 4. Seleziona tutti gli ospiti il cui nome inizia con la D
