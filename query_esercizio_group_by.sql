f9dac9103243354c52ef8a23115aa2103a51ffa2
AgneseBettio <bettio.agnese@gmail.com>
2021-06-10 17:23:23 +0200

    query 4/5

--- query_esercizio_group_by.sql
+++ query_esercizio_group_by.sql
@@ -2,18 +2,22 @@
 SELECT COUNT(`id`), YEAR(`date_of_birth`)
 FROM `ospiti` 
 GROUP BY `date_of_birth`
-ORDER BY `date_of_birth` DESC
+ORDER BY `date_of_birth` DESC;
 
 -- 2. Somma i prezzi dei pagamenti raggruppandoli per status
 SELECT SUM(`price`), `status`
 FROM `pagamenti`
-GROUP BY `status`
+GROUP BY `status`;
 
 -- 3. Conta quante volte è stata prenotata ogni stanza
 SELECT COUNT(`stanza_id`), `stanza_id`
 FROM `prenotazioni` 
-GROUP BY `stanza_id`
+GROUP BY `stanza_id`;
 
 --4. Fai una analisi per vedere se ci sono ore in cui le prenotazioni sono più frequenti
+SELECT COUNT(`id`), HOUR(`created_at`) AS `orario_prenotazione`
+FROM `prenotazioni` 
+GROUP BY HOUR(`created_at`)
+ORDER BY COUNT(`id`) DESC;
 
 --5. Quante prenotazioni ha fatto l’ospite che ha fatto più prenotazioni? (quante, non chi!)
