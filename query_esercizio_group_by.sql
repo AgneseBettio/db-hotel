32bf06d9d5fa80b444a1b2a36f668b15cb58b074
AgneseBettio <bettio.agnese@gmail.com>
2021-06-10 16:53:19 +0200

    query 2/5

--- query_esercizio_group_by.sql
+++ query_esercizio_group_by.sql
@@ -3,7 +3,11 @@ SELECT COUNT(`id`), YEAR(`date_of_birth`)
 FROM `ospiti` 
 GROUP BY `date_of_birth`
 ORDER BY `date_of_birth` DESC
+
 -- 2. Somma i prezzi dei pagamenti raggruppandoli per status
+SELECT SUM(`price`), `status`
+FROM `pagamenti`
+GROUP BY `status`
 
 -- 3. Conta quante volte è stata prenotata ogni stanza
 
