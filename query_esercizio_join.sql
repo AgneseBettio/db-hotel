3a8bb30af8a5d9575131803fbee212fe7d8a28f8
AgneseBettio <bettio.agnese@gmail.com>
2021-06-11 09:56:40 +0200

    query 4/6

--- query_esercizio_join.sql
+++ query_esercizio_join.sql
@@ -1,13 +1,12 @@
 -- 1. Come si chiamano gli ospiti che hanno fatto più di due prenotazioni?
-SELECT `prenotazioni_has_ospiti`.`ospite_id`, `prenotazioni_has_ospiti`.`prenotazione_id`,
+SELECT COUNT(`prenotazioni_has_ospiti`.`prenotazione_id`) AS `prenotazioni_per_ospite`,
         CONCAT(`ospiti`.`name`, " ", `ospiti`.`lastname`) AS `full_name`
 FROM `prenotazioni_has_ospiti` 
 LEFT JOIN `ospiti` ON `ospiti`.`id` = `prenotazioni_has_ospiti`.`ospite_id`
 LEFT JOIN `prenotazioni` ON `prenotazioni`.`id` = `prenotazioni_has_ospiti`.`prenotazione_id`
 
--- GROUP BY `prenotazioni_has_ospiti`.`ospite_id`
--- HAVING COUNT(`prenotazioni_has_ospiti`.`ospite_id`) > 2
--- non riesco ad aggiungere il count - chiedo confronto in classe
+GROUP BY `prenotazioni_has_ospiti`.`ospite_id`
+HAVING COUNT(`prenotazioni_has_ospiti`.`ospite_id`) > '2';
 
 -- 2. Stampare tutti gli ospiti per ogni prenotazione
 SELECT `prenotazioni_has_ospiti`.`ospite_id`, 
@@ -24,4 +23,13 @@ SELECT  CONCAT(`paganti`.`name`, " ", `paganti`.`lastname`) AS `full_name_pagame
 FROM `pagamenti` 
 LEFT OUTER JOIN `prenotazioni` ON `prenotazioni`.`id` = `pagamenti`.`prenotazione_id`
 LEFT OUTER JOIN `paganti` ON `paganti`.`id` = `pagamenti`.`pagante_id`
-WHERE EXTRACT(YEAR_MONTH FROM `prenotazioni`.`created_at`) = '201805'
+WHERE EXTRACT(YEAR_MONTH FROM `prenotazioni`.`created_at`) = '201805';
+
+-- Fai la somma di tutti i prezzi delle prenotazioni per le stanze del primo piano
+
+SELECT SUM(`pagamenti`.`price`) AS `earnings_1_floor`,
+       `stanze`.`floor`
+FROM `prenotazioni` 
+INNER JOIN `stanze` ON `stanze`.`id`= `prenotazioni`.`stanza_id` 
+RIGHT JOIN `pagamenti` ON `pagamenti`.`prenotazione_id`= `prenotazioni`.`id`
+WHERE `stanze`.`floor` = '1'
