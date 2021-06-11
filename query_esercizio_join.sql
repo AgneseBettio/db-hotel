390277718a5a1a7e7ed3c721f98209f5aab87d53
AgneseBettio <bettio.agnese@gmail.com>
2021-06-11 10:09:39 +0200

    query 5/6

--- query_esercizio_join.sql
+++ query_esercizio_join.sql
@@ -2,8 +2,8 @@
 SELECT COUNT(`prenotazioni_has_ospiti`.`prenotazione_id`) AS `prenotazioni_per_ospite`,
         CONCAT(`ospiti`.`name`, " ", `ospiti`.`lastname`) AS `full_name`
 FROM `prenotazioni_has_ospiti` 
-LEFT JOIN `ospiti` ON `ospiti`.`id` = `prenotazioni_has_ospiti`.`ospite_id`
-LEFT JOIN `prenotazioni` ON `prenotazioni`.`id` = `prenotazioni_has_ospiti`.`prenotazione_id`
+LEFT OUTER JOIN `ospiti` ON `ospiti`.`id` = `prenotazioni_has_ospiti`.`ospite_id`
+LEFT OUTER JOIN `prenotazioni` ON `prenotazioni`.`id` = `prenotazioni_has_ospiti`.`prenotazione_id`
 
 GROUP BY `prenotazioni_has_ospiti`.`ospite_id`
 HAVING COUNT(`prenotazioni_has_ospiti`.`ospite_id`) > '2';
@@ -25,11 +25,20 @@ LEFT OUTER JOIN `prenotazioni` ON `prenotazioni`.`id` = `pagamenti`.`prenotazion
 LEFT OUTER JOIN `paganti` ON `paganti`.`id` = `pagamenti`.`pagante_id`
 WHERE EXTRACT(YEAR_MONTH FROM `prenotazioni`.`created_at`) = '201805';
 
--- Fai la somma di tutti i prezzi delle prenotazioni per le stanze del primo piano
+-- 4. Fai la somma di tutti i prezzi delle prenotazioni per le stanze del primo piano
 
 SELECT SUM(`pagamenti`.`price`) AS `earnings_1_floor`,
        `stanze`.`floor`
 FROM `prenotazioni` 
 INNER JOIN `stanze` ON `stanze`.`id`= `prenotazioni`.`stanza_id` 
 RIGHT JOIN `pagamenti` ON `pagamenti`.`prenotazione_id`= `prenotazioni`.`id`
-WHERE `stanze`.`floor` = '1'
+WHERE `stanze`.`floor` = '1';
+
+-- 5. Prendi i dati di fatturazione per la prenotazione con id=7
+
+-- ho lasciato tutto, filtrare in select dati, come selezionarli? voglio confrontarmi con florian.
+SELECT *
+FROM `pagamenti` 
+INNER JOIN `prenotazioni` ON `prenotazioni`.`id` = `pagamenti`.`prenotazione_id`
+INNER JOIN `paganti` ON `paganti`.`id`= `pagamenti`.`pagante_id`
+WHERE `prenotazione_id` = '7';
